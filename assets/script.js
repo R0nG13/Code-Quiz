cbd594a8b85cbd8a4409eb74b92bb09b2b359120
Ron G <Ron.Gould@gmail.com>
2020-08-30 11:08:00 -0400

    initial Layout and clock

---
+++ assets/script.js
@@ -0,0 +1,48 @@
+var timerEl = document.getElementById('countdown');
+var timerTextEl = document.getElementById('timerText');
+var startBtn = document.getElementById('start');
+
+var message =
+  'Congratulations!';
+var words = message.split(' ');
+
+// Timer that counts down from 5
+function countdown() {
+  var timeLeft = 5;
+
+  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function() {
+    if (timeLeft > 1) {
+      timerEl.textContent = timeLeft + ' seconds';
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft + ' second';
+      timeLeft--;
+    } else {
+      timerEl.textContent = '';
+      clearInterval(timeInterval);
+      displayMessage();
+    }
+  }, 1000);
+}
+
+// Displays the message one word at a time
+function displayMessage() {
+  var wordCount = 0;
+
+  // Uses the `setInterval()` method to call a function to be executed every 300 milliseconds
+  var msgInterval = setInterval(function() {
+    if (words[wordCount] === undefined) {
+      clearInterval(msgInterval);
+    } else {
+      timerTextEl.textContent = words[wordCount];
+      wordCount++;
+    }
+  }, 300);
+}
+
+startBtn.onclick = countdown;
+
+
+// Questions section
+
